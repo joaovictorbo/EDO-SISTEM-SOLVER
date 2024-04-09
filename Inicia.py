cae75ba36bc1c98704e78f8eaf63ba5578938e93
joaovictorbo <118084174+joaovictorbo@users.noreply.github.com>
2024-04-09 20:15:13 -0300

    adicionando as funções porem falta troca-las

---
+++ Inicia.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+dominio(): fixa o dominio retangular no plano c = c0
+concentrations(): fixa as concentracoes cmin e cmax
+viscosidades(): fixa as viscosidades muw0(inicial da agua), muo e mug
+baricentrica(): fixa se vai usar coordenadas baricentricas ou nao
+DadosIntegracao(): fixa o comprimento do passo de integracao
+                   e o numero de pontos com passo h
+ambiente3d(): Seta o ambiente 3d e devolve "axes" para plotagem 3d para a matplotlib
+"""
+
+import matplotlib.pyplot as plt  # type: ignore
+from util.types import Real
+
+
+def dominio() -> tuple[Real, Real, Real, Real, int]:
+    # Define uma regiao R retangular para calculos.
+    # Quando tal regiao R ultrapassar os limites do triangulo, as
+    # funcoes especificas devem fazer esta restricao adicional de considerar
+    # pontos apenas na interseccao da regiao R com o triangulo
+
+    triang = 1  # 1 caso seja calculado em todo o triangulo.
+
+    umin: Real
+    umax: Real
+    vmin: Real
+    vmax: Real
+    if triang == 1:  # Calculos no triangulo todo
+        umin = 0.0
+        umax = 1.0
+        vmin = 0.0
+        vmax = 1.0
+    else:  # Calculo em regiao retangular especifica para "zoom"
+        umin = 0.2  # 0.315#0.2
+        umax = 0.6  # 0.415#0.6
+        vmin = 0.25  # 0.515#0.35
+        vmax = 0.75  # 0.630#0.75
+
+    return (umin, umax, vmin, vmax, triang)
+
+
+def concentrations() -> tuple[Real, Real]:  # Viscosidade da agua
+    cmin: Real = 0.0  # 0.5928522166980628
+    # cmax = 1.0#0.666616 #0.284124 # #1.0 Normal# L1 0.28017 #L2 e L3 0.284124
+    cmax: Real = 1.0  # 0.66#0.28017 #L1
+    return (cmin, cmax)
+
+
+def viscosidades() -> tuple[Real, Real, Real]:
+    # Viscosidades da agua, do oleo e do gas
+    muw0: Real = 1.0  # Viscosidade inicial sem polimero
+    muo: Real = 4.0  # 9.5
+    mug: Real = 0.25  # 0.45
+    return (muw0, muo, mug)
+
+
+def baricentrica() -> int:  # Mapeamento ou nao?
+    x = 1  # x = 1 se quiser coordenadas baricentricas
+    return x
+
+
+def DadosIntegracao() -> tuple[int, Real]:  # Para integracao dos contatos
+    N = 20000  # Numero de pontos na curva integral do contato para h > 0
+    h = 0.1  # Passo de integração
+    return (N, h)
+
+
+def ambiente3d() -> plt.Figure:
+    figure = plt.figure("PrismDomain")
+    axes = figure.gca(projection="3d")
+
+    return axes
+
+
+def Nniveis() -> int:
+    return 20
+
+
+####################
+# Definir funcao que armazena estilos de linhas:
+# dashed, densely dashed, solid, etc
+# from collections import OrderedDict
+
+# linestyles_dict = OrderedDict(
+#     [('solid',               (0, ())),
+#      ('loosely dotted',      (0, (1, 10))),
+#      ('dotted',              (0, (1, 5))),
+#      ('densely dotted',      (0, (1, 1))),
+
+#      ('loosely dashed',      (0, (5, 10))),
+#      ('dashed',              (0, (5, 5))),
+#      ('densely dashed',      (0, (2, 1))),
+
+#      ('loosely dashdotted',  (0, (3, 10, 1, 10))),
+#      ('dashdotted',          (0, (3, 5, 1, 5))),
+#      ('densely dashdotted',  (0, (3, 1, 1, 1))),
+
+#      ('loosely dashdotdotted', (0, (3, 10, 1, 10, 1, 10))),
+#      ('dashdotdotted',         (0, (3, 5, 1, 5, 1, 5))),
+#      ('densely dashdotdotted', (0, (3, 1, 1, 1, 1, 1)))])
+
+############################
--- plotedo.py
+++ plotedo.py
@@ -1,16 +1,140 @@
 import numpy as np
 from scipy.integrate import odeint
 import plotly.graph_objects as go
+import Inicia as ini
 
+# Concentracoes iniciais
+cmin, cmax = ini.concentrations()
+
+# Saturacoes inciais a esquerda no plano cl
+# ul, vl, cl = ini.Ul()
+
+# Saturacoes iniciais a direita no plano cr
+# ur, vr, cr = ini.Ur()
+"""
+# Viscosidades
+muwl = ini.muw(cl)
+muwr = ini.muw(cr)
+
+"""
+muw0, muo, mug = ini.viscosidades()
+
+# Define the viscosity functions
+def muw(c):
+    # Viscosity of water
+    return muw0 + c
+
+def muwc(c):
+    # dmuw/dc
+    return 1.0
+
+# Define the denominator function
+def D(u, v, c):
+    # Denominator
+    w = 1 - u - v
+    return u**2 / muw(c) + v**2 / muo + w**2 / mug
+
+# Define the partial derivatives of D
+def Du(u, v, c):
+    # dD/du
+    w = 1 - u - v
+    return 2 * u / muw(c) - 2 * w / mug
+
+def Dv(u, v, c):
+    # dD/dv
+    w = 1 - u - v
+    return 2 * v / muo - 2 * w / mug
+
+def Dc(u, v, c):
+    # dD/dc
+    return -muwc(c) * u**2 / muw(c) ** 2
 # Define the functions f, g, and a
 def f(u, v, c):
-    return u+v+c 
+    return u + v + c
+
 
 def g(u, v, c):
-    return u-v-c
+    return u - v - c
+
 
 def a(c):
-    return c+0.1
+    return c + 0.1
+
+
+def fw(u, v, c):
+    # fw
+    return (u ** 2 / muw(c)) / D(u, v, c)
+
+
+def fwu(u, v, c):
+    # dfw/du
+    return (2 * u / muw(c) * D(u, v, c) - u ** 2 / muw(c) * Du(u, v, c)) / (
+        D(u, v, c) ** 2
+    )
+
+
+def fwv(u, v, c):
+    # dfw/dv
+    return (-(u ** 2) / muw(c) * Dv(u, v, c)) / (D(u, v, c) ** 2)
+
+
+def fwc(u, v, c):
+    # dfw/dc
+    return (
+        u ** 2
+        * (-(muwc(c) / muw(c) ** 2) * D(u, v, c) - Dc(u, v, c) / muw(c))
+        / (D(u, v, c) ** 2)
+    )
+
+
+def fo(u, v, c):
+    # fo
+    return (v ** 2 / muo) / D(u, v, c)
+
+
+def fou(u, v, c):
+    # dfo/du
+    return (-(v ** 2) / muo) * Du(u, v, c) / (D(u, v, c) ** 2)
+
+
+def fov(u, v, c):
+    # dfo/dv
+    return (2 * v / muo * D(u, v, c) - v ** 2 / muo * Dv(u, v, c)) / (
+        D(u, v, c) ** 2
+    )
+
+
+def foc(u, v, c):
+    # dfo/dc
+    return -(v ** 2) / muo * Dc(u, v, c) / (D(u, v, c) ** 2)
+
+
+def fg(u, v, c):
+    # fg
+    w = 1 - u - v
+    return (w ** 2 / mug) / D(u, v, c)
+
+
+def fgu(u, v, c):
+    # dfg/du
+    w = 1 - u - v
+    return (-2 * w / mug * D(u, v, c) - w ** 2 / mug * Du(u, v, c)) / (
+        D(u, v, c) ** 2
+    )
+
+
+def fgv(u, v, c):
+    # dfg/dv
+    w = 1 - u - v
+    return (-2 * w / mug * D(u, v, c) - w ** 2 / mug * Dv(u, v, c)) / (
+        D(u, v, c) ** 2
+    )
+
+
+def fgc(u, v, c):
+    # dfg/dv
+    w = 1 - u - v
+    return (-(w ** 2) / mug * Dc(u, v, c)) / (D(u, v, c) ** 2)
 
 # Define the right-hand side of the system of ODEs
 def model(y, t, f, g, a, fR, ou, gR, ov, cR, sigma):
