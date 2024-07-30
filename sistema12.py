ec38bd75ff2ec66430779d325231589a3ae8f526
joaovictorbo <118084174+joaovictorbo@users.noreply.github.com>
2024-07-30 20:32:07 -0300

    Update sistema12.py

--- sistema12.py
+++ sistema12.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 alpha = 10**-3
 
 muw0 = 1.0  # Viscosidade inicial sem polimero
-     
+
 # Funções dadas
-def muw(c): #Viscosidade da agua
+def muw(c):  # Viscosidade da agua
     return muw0 * 2**c
 
-def muwc(c): #dmuw/dc
+def muwc(c):  # dmuw/dc
     return np.log(2) * muw0 * 2**c
 
 def muo():
@@ -22,6 +22,7 @@ def mug():
 def D(u, v, c):
     w = 1 - u - v
     return u**2 / muw(c) + v**2 / muo() + w**2 / mug()
+
 
 def f(u, v, c):
     return (u**2 / muw(c)) / D(u, v, c)
@@ -59,9 +60,6 @@ def D_dv(u, v, c):
 def D_dc(u, v, c):
     return -u**2 * muwc(c) / muw(c)**2
 
-def muwc(c):
-    return 1.0
-
 def a(c):
     return np.sqrt(c)
 
@@ -81,21 +79,45 @@ def det2(u,v,c):
     return (df_dc(u,v,c) * dg_du(u,v,c) - (df_du(u,v,c)-lambdac(u,v,c)) * dg_dc(u,v,c))
 
 def p(u, v, c):
-    return det1(u,v,c)/detalpha(u,v,c)
+    if detalpha(u, v, c) != 0:
+        return det1(u, v, c)
+    elif det1(u, v, c) != 0:
+        return 1
+    elif det2(u, v, c) != 0:
+        return det1(u, v, c)
+    else:
+        return None
 
 def q(u, v, c):
-    return det2(u,v,c)/detalpha(u,v,c)
+    if detalpha(u, v, c) != 0:
+        return det2(u, v, c)
+    elif det1(u, v, c) != 0:
+        return det2(u, v, c)
+    elif det2(u, v, c) != 0:
+        return 1
+    else:
+        return None
 
-def N(u, v, c):
-    return np.sqrt( (p(u, v, c)**2) + (q(u, v, c)**2) + 1)
+def r(u, v, c):
+    if detalpha(u, v, c) != 0:
+        return 1
+    elif det1(u, v, c) != 0:
+        return 0
+    elif det2(u, v, c) != 0:
+        return detalpha(u,v,c) /det2(u, v, c) 
+    else:
+        return None  # Singular point
 
 # Sistema de EDOs
 def system(s, y):
     u, v, c = y
-    print(u,v,c)
-    eq1 = p(u, v, c)/N(u, v, c)
-    eq2 = q(u, v, c)/N(u, v, c)
-    eq3 = 1.0/N(u, v, c)
+    eq1 = p(u, v, c) / r(u, v, c) if r(u, v, c) else None
+    eq2 = q(u, v, c) / r(u, v, c) if r(u, v, c) else None
+    eq3 = 1.0 / r(u, v, c) if r(u, v, c) else None
+    
+    if eq1 is None or eq2 is None or eq3 is None:
+        raise ValueError("Ponto singular do sistema de EDOs")
+    
     return [eq1, eq2, eq3]
 
 # Condições iniciais
